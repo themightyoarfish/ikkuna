03bb28719d58eb6289f0a77bfdf73a86a8a9550a
Rasmus Diederichsen <rasmus@peltarion.com>
2018-04-26 10:42:05 +0000

    Add training routines

--- train.py
+++ train.py
@@ -1,26 +1,31 @@
 import torch.nn as nn
+from torch.autograd import Variable
 
-from torch.utils import DataSet
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-from functools import partial
 from torch.optim import Adam
 
 def _get_optimizer(model, **kwargs):
     learning_rate = kwargs.get('learning_rate', 1e-4)
-    optimizer = kwargs.get('optimizer', Adam([p for p in model.parameters() if p.requires_grad()],
+    optimizer = kwargs.get('optimizer', Adam([p for p in model.parameters() if p.requires_grad],
                                              lr=learning_rate))
     return optimizer
 
-def train(model: nn.Module, dataset: DataSet, **kwargs):
+def train(model: nn.Module, dataset: Dataset, **kwargs):
+    model.train()
     batch_size = kwargs.get('batch_size', 1)
-    dataloader = DataLoader(dataset, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     loss_function = kwargs.get('loss', nn.CrossEntropyLoss())
     optimizer = _get_optimizer(model, **kwargs)
+    epochs = kwargs.get('epochs', 1)
 
-    for X, Y in dataloader:
-        output = model(X)
-        loss = loss_function(output, Y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        print(loss)
+    for e in range(epochs):
+        for idx, (X, Y) in enumerate(dataloader):
+            print(f'\rIteration {idx:10d} of {len(dataloader):10d}', end='')
+            data, labels = X.cuda(), Y.cuda()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_function(output, labels)
+            loss.backward()
+            optimizer.step()
+        print(f'\nEpoch {e} loss: {loss.item()}')
