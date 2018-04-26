128c7263d0b33f185717e3da9353553774be9b82
Rasmus Diederichsen <rasmus@peltarion.com>
2018-04-26 10:42:15 +0000

    Add setup

--- test_model.py
+++ test_model.py
@@ -1,10 +1,62 @@
-from models import AlexNet
+import models
+import torchvision
+from torchvision.transforms import *
+import torch
+import torch.nn as nn
+from train import train
+import numpy as np
 
 from argparse import ArgumentParser
 
+def _load_dataset(name):
+    '''Retrieve a dataset and determine the number of classes. This estimate is
+    obtained from the number of different values in the training labels'''
+    try:
+        dataset_cls = getattr(torchvision.datasets, name)
+        dataset = dataset_cls('/home/share/data',
+                              download=True,
+                              transform=ToTensor()
+                              )
+    except AttributeError:
+        raise NameError(f'Dataset {name} unknown.')
+
+    # turn labels into numpy array
+    labels = dataset.train_labels
+    if isinstance(labels, torch.Tensor):
+        labels = labels.numpy()
+    num_classes = len(np.unique(np.array(labels)))
+
+    # if only three dimensions, assume [N, H, W], else [N, H, W, C]
+    H, W = dataset.train_data.shape[1:3]
+    C = dataset.train_data.shape[-1] if len(dataset.train_data.shape) == 4 else 1
+
+    return dataset, num_classes, H, W, C
+
+def _initialize_model(module, bias_val=0.01):
+    '''Perform weight initialization on `module`. This is somewhat hacky since
+    it assumes the presence of `weight` and/or `bias` fields on the module. Will
+    skip if not present.'''
+    if hasattr(module, 'weight'):
+        nn.init.xavier_uniform_(module.weight)
+    if hasattr(module, 'bias'):
+        module.bias.data.fill_(bias_val)
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-m', '--model', type=str, choices=['AlexNet', 'DenseNet'], required=True)
-    parser.add_argument('-d', '--dataset', type=str, choices=['mnist'], required=True)
+    parser.add_argument('-m', '--model', type=str, choices=['AlexNetMini', 'DenseNet'], required=True)
+    data_choices = ['MNIST', 'FashionMNIST', 'CIFAR10', 'CIFAR100']
+    parser.add_argument('-d', '--dataset', type=str, choices=data_choices, required=True)
 
     args = parser.parse_args()
+    dataset, num_classes, H, W, C = _load_dataset(args.dataset)
+    print(f'Number of classes: {num_classes}')
+    print(f'Data shape: {(H, W, C)}')
+    model = getattr(models, args.model)((H, W, C), num_classes=num_classes)
+    model.apply(_initialize_model)
+    model.cuda()
+
+    train(model, dataset, batch_size=100, epochs=10)
+
+if __name__ == '__main__':
+    main()
