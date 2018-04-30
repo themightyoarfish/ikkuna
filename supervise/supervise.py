ad0ea8d1291b6a5c1b5b2a13f2eb436b2cf62332
Rasmus Diederichsen <rasmus@peltarion.com>
2018-04-30 10:05:27 +0000

    Extend supervisor - Add documentation - Observers for gradients and activations can now be added

--- supervise/supervise.py
+++ supervise/supervise.py
@@ -27,6 +27,7 @@ reason).
 _supervisor_stack = []
 
 from patches import nn_module
+import torch.nn as nn
 
 def supervisors():
     '''Get the stack of all supervisors'''
@@ -38,7 +39,7 @@ def current_supervisor():
     global _supervisor_stack
     return None if not _supervisor_stack else _supervisor_stack[-1]
 
-def capture_modules(*modules):
+def capture_modules(*modules, allow_subclass=False):
     '''Create a new supervisor for capturing modules.
 
     Parameters
@@ -46,24 +47,111 @@ def capture_modules(*modules):
     modules :   list(type)
                 Arbitrary types to register on creation
     '''
-    return Supervisor(allowed_modules=modules)
+    return Supervisor(modules, allow_subclass=allow_subclass)
 
 class Supervisor():
+    '''A context manager for tracking the creation of nn modules.
 
-    def __init__(self, allowed_modules=[]):
-        self._modules = []
-        self._prev_supervisor = None
-        self._allowed_modules = allowed_modules
+    Attributes
+    ----------
+    _modules    :   list
+                    List of modules created in this context manager
+    _valid_predicate    :   function
+                            Predicate based on the ``allow_subclass`` parameter for determining
+                            whether to accept a newly created module for supervision.
+    _activation_observers :   set
+                            Set of observers for activation updates
+    _gradient_observers :   set
+                            Set of observers for gradient updates
+    '''
 
-    def add_module(self, module):
+    def __init__(self, *allowed_modules, allow_subclass: bool=False):
+        '''
+        Parameters
+        ----------
+        allowed_modules :   list(type)
+                            List of module classes for supervision
+        allow_subclass  :   bool
+                            Allow calling :meth:`add_module` with subclass instances of any
+                            ``allowed_modules`` member
+
+        '''
+        self._modules         = []
+
+        if allow_subclass:
+            self._valid_predicate = lambda m: m.__class__ in allowed_modules
+        else:
+            self._valid_predicate = lambda m: any(isinstance(m, cls) for cls in allowed_modules)
+
+        self._gradient_observers   = set()
+        self._activation_observers = set()
+
+    def _check_module_is_supervised(self, module: nn.Module):
+        '''Check if a module is tracked.
+
+        Raises
+        ------
+        ValueError
+            If ``module`` is not currently supervised.
+        '''
+        if not module in self._modules:
+            raise ValueError(f'Module {module} not registered with this Supervisor')
+
+    def register_activation_observer(self, observer):
+        '''Register and observer for handling new activations.
+
+        Parameters
+        ----------
+        observer    :   object
+        '''
+        self._activation_observers.add(observer)
+
+    def register_activation_observer(self, observer):
+        '''Register and observer for handling new gradients.
+
+        Parameters
+        ----------
+        observer    :   object
+        '''
+        self._activation_observers.add(observer)
+
+    def _process_activations(self, module, in_, out_):
+        '''Hook to register on modules for receiving activation updates. See
+        :meth:`nn.Module.register_forward_hook`.
+
+        Raises
+        ------
+        ValueError
+            If ``module`` was not previously added with :meth:`add_module`.
+        '''
+        self._check_module_is_supervised(module)
+        for o in self._activation_observers:
+            o.process_activations(module, out_)
+
+    def _process_gradients(self, module, grad_in_, grad_out_):
+        '''Hook to register on modules for receiving gradient updates. See
+        :meth:`nn.Module.register_backward_hook`
+
+        Raises
+        ------
+        ValueError
+            If ``module`` was not previously added with :meth:`add_module`.
+        '''
+        self._check_module_is_supervised(module)
+        for o in self._gradient_observers:
+            o.process_gradients(module, grad_out_)
+
+    def add_module(self, module: nn.Module):
+        '''Add a module to supervise.'''
         # attention: This call can be executed before the module is fully constructed, in case
-        # nn.Module is patched. In that case, the concrete module's initializer will call into the
-        # super class and we land here. So do not reference any object attributes here, only class
-        # ones.
+        # nn.Module.__init__ is patched to call this method. In that case, the concrete module's
+        # initializer will call into the super class and we land here. So do not reference any
+        # object attributes here, only class ones.
         # TODO: Think about the safety of this injection some more for production purposes
-        if module.__class__ in self._allowed_modules:
-            print(f'Adding {module.__class__.__name__}')
+        if self._valid_predicate(module):
             self._modules.append(module)
+            module.register_forward_hook(self._process_activations)
+            module.register_backward_hook(self._process_gradients)
 
     def __enter__(self):
         global _supervisor_stack
@@ -71,4 +159,5 @@ class Supervisor():
 
     def __exit__(self, exc_type, exc_val, traceback):
         global _supervisor_stack
+        assert self == _supervisor_stack[-1]    # sanity check
         _supervisor_stack.pop()
