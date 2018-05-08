09e11ea1ed6664fb9e24c62ac4d0a22f25b1d634
Rasmus Diederichsen <rasmus@peltarion.com>
2018-05-08 08:52:07 +0000

    Add supervisor to densenet

--- models/densenet.py
+++ models/densenet.py
@@ -47,9 +47,12 @@ class _Transition(nn.Sequential):
 
 class DenseNet(nn.Module):
     def __init__(self, input_shape, growth_rate=32, block_config=(6, 12),
-                 num_init_features=32, bn_size=4, drop_rate=0, num_classes=1000):
+                 num_init_features=32, bn_size=4, drop_rate=0, num_classes=1000, supervisor=None):
 
         super(DenseNet, self).__init__()
+
+        # create empty supervisor if none given
+        self._supervisor = supervisor or Supervisor()
 
         # if batch dim not present, add 1
         if len(input_shape) == 2:
@@ -57,32 +60,33 @@ class DenseNet(nn.Module):
         H, W, C = input_shape
 
         # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(C, num_init_features, kernel_size=7, stride=1, padding=3,
-                                bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
+        with self._supervisor:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(C, num_init_features, kernel_size=7, stride=1, padding=3,
+                                    bias=False)),
+                ('norm0', nn.BatchNorm2d(num_init_features)),
+                ('relu0', nn.ReLU(inplace=True)),
+                ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+            ]))
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
-                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
+            # Each denseblock
+            num_features = num_init_features
+            for i, num_layers in enumerate(block_config):
+                block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
+                                    bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
+                self.features.add_module('denseblock%d' % (i + 1), block)
+                num_features = num_features + num_layers * growth_rate
+                if i != len(block_config) - 1:
+                    trans = _Transition(num_input_features=num_features,
+                                        num_output_features=num_features // 2)
+                    self.features.add_module('transition%d' % (i + 1), trans)
+                    num_features = num_features // 2
 
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+            # Final batch norm
+            self.features.add_module('norm5', nn.BatchNorm2d(num_features))
 
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
+            # Linear layer
+            self.classifier = nn.Linear(num_features, num_classes)
 
     def forward(self, x):
         features = self.features(x)
