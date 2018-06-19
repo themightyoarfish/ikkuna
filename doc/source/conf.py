53fb15f9aec56f30ead8ed588340d5184af891b7
Rasmus Diederichsen <rasmus@peltarion.com>
2018-06-19 14:24:02 +0200

    Remove special memeber from doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -172,4 +172,4 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-autodoc_default_flags = ['special-members', 'private-members']
+autodoc_default_flags = ['private-members']
