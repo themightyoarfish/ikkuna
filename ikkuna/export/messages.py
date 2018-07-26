4f490bd7b9d79e733ac0057fd5c0b28a3db6c6c2
Rasmus Diederichsen <rasmus@peltarion.com>
2018-07-26 14:02:16 +0200

    Add documentaion for NetworkData

--- ikkuna/export/messages.py
+++ ikkuna/export/messages.py
@@ -1,5 +1,21 @@
 from collections import namedtuple
 
 
-NetworkData = namedtuple('NetworkData', ['tag', 'seq', 'step', 'epoch', 'kind', 'module',
-                                         'payload'])
+class NetworkData(namedtuple('NetworkData', ['tag', 'seq', 'step', 'epoch', 'kind', 'module',
+                                             'payload'])):
+    '''
+    Attributes
+    ----------
+    tag :   str
+            The tag associated with this message
+    seq :   int
+            Global sequence number. This counter should not reset after each epoch.
+    step    :   int
+                Epoch-local sequence number (the current batch index)
+    epoch   :   int
+                Current epoch number
+    kind    :   str
+                Message kind
+    module  :   ikkuna.utils.NamedModule
+    payload :   torch.Tensor
+    '''
