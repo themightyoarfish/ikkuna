318c014dc30eebbb2d4c65bbdb658e5e5a979b97
Rasmus Diederichsen <rasmus@peltarion.com>
2018-08-23 11:20:23 +0200

    make load_dataset available from ikkuna.utils

--- ikkuna/utils/__init__.py
+++ ikkuna/utils/__init__.py
@@ -2,4 +2,5 @@ from .utils import *
 from .module_tree import ModuleTree
 from .named_module import NamedModule
 
-__all__ = ['ModuleTree', 'NamedModule', 'make_fill_polygons', 'available_optimizers', 'create_optimizer', 'initialize_model']
+__all__ = ['ModuleTree', 'NamedModule', 'make_fill_polygons', 'available_optimizers',
+           'create_optimizer', 'initialize_model', 'load_dataset']
