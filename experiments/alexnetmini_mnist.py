689cde9cda98a77c9247fdaebb0f87a653604dc1
Rasmus Diederichsen <rasmus@peltarion.com>
2018-09-03 08:26:52 +0200

    some updates to alexnet experiment

--- experiments/alexnetmini_mnist.py
+++ experiments/alexnetmini_mnist.py
@@ -1,34 +1,32 @@
 import sys
 sys.path.append('.')
-import random
-import numpy as np
 from tqdm import tqdm
 import torch
-from torchvision.transforms import Lambda, ToTensor
 
 from ikkuna.export.subscriber import (TrainAccuracySubscriber, TestAccuracySubscriber,
                                       SpectralNormSubscriber, RatioSubscriber)
 from ikkuna.export import Exporter
-from ikkuna.utils import load_dataset
+from ikkuna.utils import load_dataset, seed_everything
 from ikkuna.models import AlexNetMini
+from ikkuna.visualization import configure_prefix
+configure_prefix('alexnetmini_mnist')
 from train import Trainer
 from schedulers import FunctionScheduler
+seed_everything()
 
 
 def schedule_fn(base_lrs, batch, step, epoch):
-    return  base_lrs
+    return base_lrs
     # return [base_lr * 0.96 ** epoch for base_lr in base_lrs]
 
 
 train_config = {
-    'base_lr':       0.1,
-    'optimizer':     'SGD',
-    'weight_decay':  0.0001,
-    'momentum':      0.9,
-    'batch_size':    256,
-    'n_epochs':      10,
-    'loss':          torch.nn.CrossEntropyLoss(),
-    'schedule':      schedule_fn,
+    'base_lr':      0.01,
+    'optimizer':    'SGD',
+    'batch_size':   256,
+    'n_epochs':     30,
+    'loss':         torch.nn.CrossEntropyLoss(),
+    'schedule':     schedule_fn,
 }
 
 if __name__ == '__main__':
@@ -41,8 +39,7 @@ def main():
                       loss=train_config['loss'], exporter=exporter)
     model = AlexNetMini(dataset_train.shape[1:], num_classes=10, exporter=exporter)
     trainer.set_model(model)
-    trainer.optimize(name=train_config['optimizer'], weight_decay=train_config['weight_decay'],
-                     momentum=train_config['momentum'], lr=train_config['base_lr'])
+    trainer.optimize(name=train_config['optimizer'], lr=train_config['base_lr'])
     trainer.set_schedule(FunctionScheduler, train_config['schedule'])
     trainer.add_subscriber(TrainAccuracySubscriber())
     trainer.add_subscriber(TestAccuracySubscriber(dataset_test, trainer.model.forward,
