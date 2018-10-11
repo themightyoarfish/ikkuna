b1781afef629d7bf13622c87f0bcc8d475d52092
Rasmus Diederichsen <rasmus@peltarion.com>
2018-10-11 10:29:13 +0000

    Don't fuck with diagram orientation

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -13,19 +13,16 @@ do
     (
     fname="ikkuna.$subpackage"
     echo "Writing dot file to $dotdir/$fname.dot"
-    pyreverse -o dot --module-names=y --only-classnames -p $fname ikkuna/$subpackage
+    pyreverse -o dot --filter-mode=ALL --module-names=y -p $fname ikkuna/$subpackage > /dev/null
 
     # classes_… is always generated
     mv classes_$fname.dot $dotdir
-    # somehow, passing -Grankdir=LR doesn't to jack shit, so use sed #yolo
-    sed -i "s/rankdir=../rankdir=LR/g" $dotdir/classes_$fname.dot
     echo "Generating class diagram for $fname"
     dot -Tpdf -Nfontname=DejaVuSansMono -Efontname=DejaVuSansMono $dotdir/classes_$fname.dot > "$dotdir/classes_ikkuna.$subpackage.pdf"
 
     # packages_… file is not generated for every file
     if [ -f packages_$fname.dot ]; then
         mv packages_$fname.dot $dotdir
-        sed -i "s/rankdir=../rankdir=LR/g" $dotdir/packages_$fname.dot
         echo "Generating package diagram for $fname"
         dot -Tpdf -Nfontname=DejaVuSansMono -Efontname=DejaVuSansMono $dotdir/packages_$fname.dot > "$dotdir/packages_ikkuna.$subpackage.pdf"
     fi
