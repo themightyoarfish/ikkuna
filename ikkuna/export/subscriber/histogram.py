3a498f1c72c97d49ab788a87b9a9f0b10a1e38d8
Rasmus Diederichsen <rasmus@peltarion.com>
2018-10-29 14:32:43 +0000

    Remove useless attr; fixes #20; formatting; fixes #19

--- ikkuna/export/subscriber/histogram.py
+++ ikkuna/export/subscriber/histogram.py
@@ -6,11 +6,6 @@ class HistogramSubscriber(PlotSubscriber):
 
     '''A :class:`~ikkuna.export.subscriber.Subscriber` which subsamples training artifacts and
     computes histograms per epoch.  Histograms are non-normalized.
-
-    Attributes
-    ----------
-    _buffer :   dict(list)
-                Per-module buffer of values to compute more robust histogram
     '''
 
     def __init__(self, kinds, tag=None, subsample=1, backend='tb'):
@@ -18,10 +13,9 @@ class HistogramSubscriber(PlotSubscriber):
         title        = f'{kinds[0]} histogram'
         ylabel       = 'Frequency'
         super().__init__(subscription, {'title': title, 'ylabel': ylabel}, backend=backend)
-        self._buffer = defaultdict(list)
 
     def _metric(self, message_bundle):
 
         module_name = message_bundle.identifier
-        data   = message_bundle.data[self._subscription.kinds[0]]
+        data        = message_bundle.data[self._subscription.kinds[0]]
         self._backend.add_histogram(module_name, data, message_bundle.seq)
