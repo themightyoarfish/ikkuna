2aa3165cd2732466c312ab9f244559ff7171cd88
Rasmus Diederichsen <rasmus@peltarion.com>
2018-11-12 13:22:59 +0100

    Add VGG model

--- ikkuna/models/__init__.py
+++ ikkuna/models/__init__.py
@@ -2,5 +2,6 @@ from .alexnetmini import AlexNetMini
 from .densenet import DenseNet
 from .resnet import ResNet
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from .vgg import VGG
 
-__all__ = ['AlexNetMini', 'DenseNet', 'ResNet']
+__all__ = ['AlexNetMini', 'DenseNet', 'ResNet', 'VGG']
