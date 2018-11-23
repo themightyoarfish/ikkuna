94c99ac68d06a3440f420b534b3c1c2a20ea15d3
Rasmus Diederichsen <rasmus@peltarion.com>
2018-11-23 13:03:16 +0100

    Introduce reusable experiment runner

--- experiments/learning_rate/alexnetmini/experiment_validation_runner.py
+++ experiments/learning_rate/alexnetmini/experiment_validation_runner.py
@@ -1,15 +1,13 @@
-from experiments.learning_rate.alexnetmini.experiment_validation import ex, schedules
+from experiments.learning_rate.alexnetmini.experiment_validation import schedules
+from experiments.runner import Runner
 
 import itertools
 import os
-from subprocess import Popen, DEVNULL
 from argparse import ArgumentParser
-import time
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-p', '--parallel', action='store_true', help='Run experiments to run')
     parser.add_argument('-n', '--nruns', type=int, default=5, help='Number of runs per config')
     parser.add_argument('-j', '--jobs', type=int, default=3, help='Number of processes to use')
     args = parser.parse_args()
@@ -21,53 +19,11 @@ def main():
 
     experiment_scriptname = os.path.join(os.path.dirname(__file__), 'experiment_validation.py')
 
-    if args.parallel:
-        print('Running parallel')
-        running = set()
+    runner = Runner(args.jobs)
+    for (schedule, lr, bs) in runs:
+        runner.submit(experiment_scriptname, schedule=schedule, base_lr=lr, batch_size=bs)
 
-        # shortcut to pop a run config from the list and start a process with it
-        def start_job():
-            schedule, lr, bs = runs.pop()
-            running.add(Popen(['python', experiment_scriptname, 'with', f'schedule={schedule}',
-                               f'base_lr={lr}', f'batch_size={bs}'],
-                              stdout=DEVNULL))
-
-        # do not start more procs than configs
-        procs_to_start = min(len(runs), args.jobs)
-
-        try:
-            # run initial batch
-            print(f'Starting {procs_to_start} jobs')
-            for i in range(procs_to_start):
-                start_job()
-
-            # keep polling the list of running processes. if one has terminated, remove it and start
-            # a new one, unless the list of run configs is empty. In that case, if there are no more
-            # running processes, exit the loop, otherwise simply remove the terminated process.
-            while True:
-                # all done?
-                if not runs and not running:
-                    break
-
-                for proc in running:
-                    if proc.poll() is not None:
-                        if runs:
-                            print('Starting new job.')
-                            start_job()
-                            running.remove(proc)
-                            print(f'{len(runs)} left.')
-                        else:
-                            running.remove(proc)
-                time.sleep(10)
-        except Exception as e:
-            print(e)
-            print('Attempting to cancel all processes...')
-            for proc in running:
-                proc.terminate()
-    else:
-        print('Running serial')
-        for schedule in runs:
-            ex.run(config_updates={'schedule': schedule})
+    runner.run()
 
 
 if __name__ == '__main__':
---
+++ experiments/runner.py
@@ -0,0 +1,69 @@
+from subprocess import Popen, DEVNULL
+import time
+
+
+class Job(object):
+
+    def __init__(self, scriptname, updates):
+        self._scriptname = scriptname
+        self._updates    = [f'{name}={value}' for name, value in updates.items()]
+        self._process    = None
+
+    def create(self):
+        self._process = Popen(['python', self._scriptname, 'with'] + self._updates, stdout=DEVNULL)
+
+    def poll(self):
+        return self._process.poll()
+
+    def terminate(self):
+        return self._process.terminate()
+
+
+class Runner(object):
+
+    def __init__(self, n_parallel):
+        self._n_parallel = n_parallel
+        self._jobs       = set()
+        self._running    = set()
+
+    def submit(self, scriptname, **conf_updates):
+        self._jobs.add(Job(scriptname, conf_updates))
+
+    def run(self):
+        # do not start more procs than configs
+        procs_to_start = min(len(self._jobs), self._n_parallel)
+
+        def start_job():
+            job = self._jobs.pop()
+            job.create()
+            self._running.add(job)
+
+        try:
+            # run initial batch
+            print(f'Starting {procs_to_start} jobs')
+            for i in range(procs_to_start):
+                start_job()
+
+            # keep polling the list of running processes. if one has terminated, remove it and start
+            # a new one, unless the list of run configs is empty. In that case, if there are no more
+            # running processes, exit the loop, otherwise simply remove the terminated process.
+            while True:
+                # all done?
+                if len(self._jobs) == 0 and not self._running:
+                    break
+
+                for proc in self._running:
+                    if proc.poll() is not None:
+                        if len(self._jobs) > 0:
+                            print('Starting new job.')
+                            start_job()
+                            self._running.remove(proc)
+                            print(f'{len(self._jobs)} left.')
+                        else:
+                            self._running.remove(proc)
+                time.sleep(5)
+        except Exception as e:
+            print(e)
+            print('Attempting to cancel all processes...')
+            for proc in self._running:
+                proc.terminate()
