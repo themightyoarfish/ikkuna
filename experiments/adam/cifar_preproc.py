ac2943c9686aafcf1543b65c7436ae8c3d72ef55
Rasmus Diederichsen <rasmus@peltarion.com>
2018-12-24 09:10:04 +0100

    Add whitening functions (don't work for some reasons)

--- experiments/adam/cifar_preproc.py
+++ experiments/adam/cifar_preproc.py
@@ -5,36 +5,75 @@ import numpy as np
 from ikkuna.utils import load_dataset
 
 
-dataset = load_dataset('CIFAR10')
+dataset_train, dataset_test = load_dataset('CIFAR10')
+
+def zca_whitening_matrix(X):
+    '''Compute the ZCA whitening transform.
+
+    Parameters
+    ----------
+    X   :   np.ndarray
+            Data matrix of shape (n_samples, n_features)
+
+    Returns
+    -------
+    np.ndarray
+        (n_features, n_features) whitening matrix. Use it for whitening by X.dot(ZCAMatrix) to
+        obtain data with unit variance and zero covariance between features. (Not exactly, but more
+        or less)
+    '''
+    # Compute the covariance
+    N         = X.shape[0]
+    mean      = X.mean(axis=0)
+    sigma     = (X - mean).T.dot((X - mean)) / N
+    # Decompose
+    U, S, V   = np.linalg.svd(sigma)
+    epsilon   = 1e-5
+    ZCAMatrix = np.dot(U, np.dot(np.diag(1.0 / np.sqrt(S + epsilon)), U.T))
+    return ZCAMatrix
 
 
-def compute_covariance_matrix(dataset):
-    raw_data = dataset.dataset.data
+def compute_covariance_matrix(raw_data):
     N        = raw_data.shape[0]
     raw_data = raw_data / raw_data.max()
-    raw_data = raw_data.reshape(N, -1)
     mean     = raw_data.mean(axis=0)
     raw_data = raw_data - mean
-    cov      = raw_data.T.dot(raw_data) / (N - 1)
+    cov      = raw_data.T.dot(raw_data) / N
     return cov
 
 
 if __name__ == '__main__':
-    if os.path.exists('cifar_cov.npy'):
+    data_train = dataset_train.dataset.data.reshape(50000, -1).astype(np.float)[:3, ...]
+    data_test = dataset_test.dataset.data.reshape(10000, -1).astype(np.float)[:3, ...]
+    data = np.concatenate([data_train, data_test])
+    if False and os.path.exists('cifar_cov.npy'):
         print('Loading covariance matrix')
         cov = np.load('cifar_cov.npy')
     else:
         print('Computing covariance matrix')
-        cov = compute_covariance_matrix(dataset[0])
+        cov = compute_covariance_matrix(data)
         print('Saving covariance matrix')
         np.save('cifar_cov.npy', cov)
 
-    if os.path.exists('zca_matrix.npy'):
+    if False and os.path.exists('zca_matrix.npy'):
+        print('Loading whitening matrix')
         ZCAMatrix = np.load('zca_matrix.npy')
     else:
-        U, S, V   = np.linalg.svd(cov)
-        epsilon   = 1e-5
-        ZCAMatrix = np.dot(U, np.dot(np.diag(1.0/np.sqrt(S + epsilon)), U.T))
+        print('Computing whitening transform')
+        ZCAMatrix = zca_whitening_matrix(data)
         np.save('zca_matrix.npy', ZCAMatrix)
 
-    __import__('ipdb').set_trace()
+    print('Whitening data')
+    whitened_data = data.dot(ZCAMatrix).astype(np.uint8).reshape(-1, 3, 32, 32)
+    whitened_data_train = whitened_data[:3, ...]
+    whitened_data_test = whitened_data[3:, ...]
+    np.save('whitened_cifar_train.npy', whitened_data_train)
+    np.save('whitened_cifar_test.npy', whitened_data_test)
+
+    import matplotlib.pyplot as plt
+    orig_img = dataset_train.dataset.data[0]
+    plt.figure()
+    plt.imshow(orig_img)
+    plt.figure()
+    plt.imshow(whitened_data_train[0, ...].transpose())
+    plt.show()
