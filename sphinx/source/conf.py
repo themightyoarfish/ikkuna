954fdac3f073080c8faaf4c3a2f0e3a477f5ed66
Rasmus Diederichsen <rasmus@peltarion.com>
2019-03-02 17:48:29 +0100

    Regen docs with new logo

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -102,7 +102,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = html_static_path[0] + '/logo.png'
+html_logo = html_static_path[0] + '/ikkuna_logo.svg'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
